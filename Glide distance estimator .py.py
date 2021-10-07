1c0d41ba6cff321386fb38bcbb04429d1beb9748
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2021-10-07 09:31:36 -0400

    Update Glide distance estimator .py.py
    Changes were made to make it easier to bring up index or exit.

--- Glide distance estimator .py.py
+++ Glide distance estimator .py.py
@@ -1,46 +1,56 @@
 glide_ratio = {
-    "i":          0,
-    "aircraft a": 15,
-    "aircraft b": 9,
-    "aircraft c": 17,
-    "aircraft d": 7,
-    "aircraft e": 16,
-    "aircraft f": 10,
-    "x":          0,       
+    "aircraft a": 8,          
+    "aircraft b": 15,
+    "aircraft c": 9,
+    "aircraft d": 17,
+    "aircraft e": 7,
+    "aircraft f": 16,
+    "aircraft g": 10,
+    "aircraft h": 6,                 
 }   
 
 def ind():
-    print("   index           ")
-    print("   aircraft a      ") 
-    print("   aircraft b      ")
+    print("   aircraft a      ")
+    print("   aircraft b      ") 
     print("   aircraft c      ")
     print("   aircraft d      ")
-    print("   aircraft e      ") 
+    print("   aircraft e      ")
     print("   aircraft f      ") 
+    print("   aircraft g      ")
+    print("   aircraft h      ")
     
 
 ind()
-print("Enter i for aircraft and 0 for altitude for index.")
-print("Enter x for aircraft and 0 for altitude to exit.")
+print("Enter a aircraft from index and a altitude to get the estamated glide distance.")
+print("Enter i for index and x to exit.")
 while True:
     try:
         print("Enter a aircraft in index.")
-        search = input()  
+        search = input()
+        if search == "i":
+            ind()
+            continue
+        elif search == "x":
+            break
         print("Enter a altitude")
         altitude = input()
+        if altitude == "i":
+            ind()
+            continue
+        elif altitude == "x":
+            break
         result = float(altitude) *   float(glide_ratio[search]) / 5280 
         print(result)
         print("miles")
-        if   search  == "i" :
-            ind()
-        elif search == "x":
-            break
     except KeyError:
         print("Enter a valid aircraft name.")
         print("Try again.")
     except ValueError:   
         print("Enter a valid altitude.")
-        print("Try again.")     
+        print("Try again.")       
+        
+        
+      
     
 
 
