28c2aa860493b1fe0f746b92abe8a5767c617ec6
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-02-23 07:53:01 -0500

    Add files via upload

---
+++ Glide distance estimator fpurge new.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <string.h>
+
+void ind(){
+	     printf("\n");
+	     printf("     aircraft a\n"); 
+	     printf("     aircraft b\n");       
+	     printf("     aircraft c\n");       
+	     printf("     aircraft d\n");       
+	     printf("     aircraft e\n");
+	     printf("     aircraft f\n");
+	     printf("     aircraft g\n");
+	     printf("     aircraft h\n\n");
+             }	
+
+
+int main(){
+
+       	char ac [8]  [20] = {"aircraft a","aircraft b","aircraft c","aircraft d","aircraft e","aircraft f","aircraft g","aircraft h"}; 
+	int gr [] = { 15, 9, 17, 7,16,10, 17,15};
+
+    char aircraft [20];
+    double glide_ratio;
+    double alt;
+
+ while(1){
+printf("Enter a aircraft, x to exit or i for index.\n");
+	fgets(aircraft,20,stdin);
+	aircraft[strcspn(aircraft, "\n")] = 0;
+	__fpurge(stdin);
+	if(strcmp(aircraft,"i")==0){
+	ind();
+	continue;}
+	else if(strcmp(aircraft,"x")==0){
+	break;}
+ else if(strcmp(aircraft,ac[0])==0){
+ glide_ratio = gr[0];
+}else if(strcmp(aircraft,ac[1])==0){
+ glide_ratio = gr[1];
+}else if(strcmp(aircraft,ac[2])==0){
+ glide_ratio = gr[2];
+}else if(strcmp(aircraft,ac[3])==0){
+ glide_ratio = gr[3];
+}else if(strcmp(aircraft,ac[4])==0){
+ glide_ratio = gr[4];
+}else if(strcmp(aircraft,ac[5])==0){
+ glide_ratio = gr[5];
+}else if(strcmp(aircraft,ac[6])==0){
+ glide_ratio = gr[6];
+}else if(strcmp(aircraft,ac[7])==0){
+ glide_ratio = gr[7];}
+ else{
+     printf("Enter a aircraft from index only.\n");
+     printf("\n");
+     continue;}	
+	printf("Enter a altitude to get a estimated straight line glide distance.\n");
+        while(scanf("%lf", &alt)!=1){
+	__fpurge(stdin);
+        printf("Enter a valid altitude.\n");
+        continue;}
+        __fpurge(stdin);	
+	printf("\nThe glide distance is %f",alt * glide_ratio  / 5280);
+	printf(" miles.\n\n");
+     }
+return 0;
+}
+	
