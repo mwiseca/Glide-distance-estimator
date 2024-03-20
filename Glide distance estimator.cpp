4b54276b5d54b3aa95855b549d132fd52384e890
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-03-20 10:22:38 -0400

    Add files via upload

---
+++ Glide distance estimator.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <map>
+void ind(){
+using namespace std;
+ cout << "               \n";
+ cout << "     aircraft a\n";
+ cout << "     aircraft b\n";
+ cout << "     aircraft c\n";
+ cout << "     aircraft d\n";
+ cout << "     aircraft e\n";
+ cout << "     aircraft f\n";
+ cout << "     aircraft g\n";
+ cout << "                \n";
+}
+
+
+int main(){
+   std::map<std::string,double> gr = {
+       {"aircraft a",      15},
+       {"aircraft b",       9},
+       {"aircraft c",      17},
+       {"aircraft d",       7},
+       {"aircraft e",      10},
+       {"aircraft f",      17},
+       {"aircraft g",      15}
+};
+       std::string search;
+       std::string altitude; 
+
+while(true){
+         try{ 
+	  std::cout << "Enter a aircraft name, i for index x to exit.\n";
+	  getline(std::cin,search);
+	  if(search == "x"){
+	  break;}
+	  else if(search == "i"){
+	  ind();
+	  continue;}
+	  std::cout << "Enter a altitude.\n";
+	  getline(std::cin,altitude);
+	  if(altitude == "i"){
+	  ind();
+	  continue;}
+	  else if(altitude == "x"){
+	  break;}
+	  double result = stod(altitude) *   (gr[search]) / 5280;
+          std::cout << "The estimated glide distance is " << result << " miles.\n\n";
+	}catch(const std::invalid_argument &e){
+	 std::cout << "Enter a valid altitude.\n\n";
+	}
+
+}
+
+return 0;
+}
