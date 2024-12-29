24e914180a8c26285055b3a14ceb785f7b56b023
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2024-12-29 18:44:26 -0500

    Add files via upload

--- Glide distance estimator-C.cpp
+++ Glide distance estimator-C.cpp
@@ -1,78 +1,79 @@
+#include <cstdlib>
+#include <iostream>
+#include <map>
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
 #include <string>
-#include <cstdlib>
-#include <map>
-void ind(){
-   
- printf("               \n");
- printf("     aircraft a\n");
- printf("     aircraft b\n");
- printf("     aircraft c\n");
- printf("     aircraft d\n");
- printf("     aircraft e\n");
- printf("     aircraft f\n");
- printf("     aircraft g\n");
- printf("     aircraft h\n"); 
+void ind() {
+
+    printf("               \n");
+    printf("     aircraft a\n");
+    printf("     aircraft b\n");
+    printf("     aircraft c\n");
+    printf("     aircraft d\n");
+    printf("     aircraft e\n");
+    printf("     aircraft f\n");
+    printf("     aircraft g\n");
+    printf("     aircraft h\n");
 }
 
+int main() {
+    std::map<std::string, double> gr = {    
+        {"aircraft a", 15},                                       
+        {"aircraft b",  9},
+        {"aircraft c", 17},
+        {"aircraft d",  7},
+        {"aircraft e", 16},
+        {"aircraft f", 10},
+        {"aircraft g", 17},
+        {"aircraft h", 15}
+    };
+            
+    char search[2000];
+    char altitude[2000];
 
-int main(){
-   std::map<std::string,double> gr = {
-       {"aircraft a",      15},
-       {"aircraft b",       9},
-       {"aircraft c",      17},
-       {"aircraft d",       7},
-       {"aircraft e",      16},
-       {"aircraft f",      10},
-       {"aircraft g",      17},
-       {"aircraft h",      15} 
-};  
+    printf("Enter a aircraft from index and a altitude to get the estimated glide distance.\n");
+    printf("Enter i for index and x to exit.\n");
+    while (1) {
 
-       char search [2000];
-       char altitude [2000]; 
-
-
- printf("Enter a aircraft from index and a altitude to get the estimated glide distance.\n");
- printf("Enter i for index and x to exit.\n");
-while(1){
-        
-	  printf("Enter a aircraft in index.\n");
-	  while(fgets(search,2000,stdin)){
-	  search[strcspn(search,"\n")]=0;
-	  auto c = gr.find(search);
-	  if(strcmp(search,"x")==0){
-	  exit(EXIT_SUCCESS);}
-	  else if(strcmp(search,"i")==0){
-	  ind();
-	  printf("Enter a aircraft in index.\n");
-	  continue;}
-	  else if(c == gr.end()){
-          std::cout << "Enter a aircraft in index only.\n\n";
-          continue;}
-	  else{
-		 break;} 
-          }          	  
-	    printf("Enter a altitude.\n");
-	    while(fgets(altitude,2000,stdin)){
-            altitude[strcspn(altitude,"\n")]=0;
-            if(strcmp(altitude,"i")==0){
-            ind();
-	    printf("Enter a altitude.\n");
-            continue;}
-            else if(strcmp(altitude,"x")==0){
-            exit(EXIT_SUCCESS);}
-            double result = atof(altitude) *   gr.at(search) / 5280;
-            if(atof(altitude) * gr.at(search) / 5280 == 0){
+        printf("Enter a aircraft in index.\n");
+        while (fgets(search, 2000, stdin)) {
+            search[strcspn(search, "\n")] = 0;
+            auto c = gr.find(search);
+            if (strcmp(search, "x") == 0) {
+                exit(EXIT_SUCCESS);
+            } else if (strcmp(search, "i") == 0) {
+                ind();
+                printf("Enter a aircraft in index.\n");
+                continue;
+            } else if (c == gr.end()) {
+                std::cout << "Enter a aircraft in index only.\n\n";
+                continue;
+            } else {
+                break;
+            }
+        }
+        printf("Enter a altitude.\n");
+        while (fgets(altitude, 2000, stdin)) {
+            altitude[strcspn(altitude, "\n")] = 0;
+            if (strcmp(altitude, "i") == 0) {
+                ind();
+                printf("Enter a altitude.\n");
+                continue;
+            } else if (strcmp(altitude, "x") == 0) {
+                exit(EXIT_SUCCESS);
+            }
+            double result = atof(altitude) * gr.at(search) / 5280;
+            if (atof(altitude) * gr.at(search) / 5280 == 0) {
                 printf("\nEnter a valid altitude only.\n\n");
-               continue;}
-	    else{
-		 break;}
-	    }
-            double result = atof(altitude) *   gr.at(search) / 5280; 
-            printf("\nThe estimated glide distance is %lf miles.\n\n",result);
-       	
+                continue;
+            } else {
+                break;
+            }
+        }
+        double result = atof(altitude) * gr.at(search) / 5280;
+        printf("\nThe estimated glide distance is %lf miles.\n\n", result);
+    }
+    return 0;
 }
-return 0;
-}
+
--- Glide distance estimator2.cpp
+++ Glide distance estimator2.cpp
@@ -1,69 +1,71 @@
-
 #include <iostream>
-#include <string>
 #include <map>
-void ind(){
- using namespace std;
- cout << "               \n";
- cout << "     aircraft a\n";
- cout << "     aircraft b\n";
- cout << "     aircraft c\n";
- cout << "     aircraft d\n";
- cout << "     aircraft e\n";
- cout << "     aircraft f\n";
- cout << "     aircraft g\n";
- cout << "                \n";
+#include <string>
+void ind() {
+    using namespace std;
+    cout << "               \n";
+    cout << "     aircraft a\n";
+    cout << "     aircraft b\n";
+    cout << "     aircraft c\n";
+    cout << "     aircraft d\n";
+    cout << "     aircraft e\n";
+    cout << "     aircraft f\n";
+    cout << "     aircraft g\n";
+    cout << "               \n";
 }
 
+int main() {
+    std::map<std::string, double> gr = {      
+        {"aircraft a", 15},
+        {"aircraft b",  9},
+        {"aircraft c", 17},
+        {"aircraft d",  7},
+        {"aircraft e", 10},
+        {"aircraft f", 17},
+        {"aircraft g", 15}
+    };
+    
+    std::string search;
+    std::string altitude;
 
-int main(){
-   std::map<std::string,double> gr = {
-       {"aircraft a",      15},
-       {"aircraft b",       9},
-       {"aircraft c",      17},
-       {"aircraft d",       7},
-       {"aircraft e",      10},
-       {"aircraft f",      17},
-       {"aircraft g",      15}
-};
-       std::string search;
-       std::string altitude; 
+    std::cout << "Enter a aircraft from index and a altitude to get the estimated glide distance.\n";
+    std::cout << "Enter i for index and x to exit.\n";
+    while (true) {
+        std::cout << "Enter a aircraft in index.\n";
+        while (getline(std::cin, search)) {
+            auto i = gr.find(search);
+            if (search == "x") {
+                exit(EXIT_SUCCESS);
+            } else if (search == "i") {
+                ind();
+                std::cout << "Enter a aircraft in index.\n";
+                continue;
+            } else if (i == gr.end()) {
+                std::cout << "\nEnter a aircraft in index only.\n\n";
+                continue;
+            } else {
+                break;
+            }
+        }
+        std::cout << "Enter a altitude.\n";
+        while (getline(std::cin, altitude)) {
+            try {
+                if (altitude == "i") {
+                    ind();
+                    std::cout << "Enter a altitude.\n";
+                    continue;
+                } else if (altitude == "x") {
+                    exit(EXIT_SUCCESS);
+                }
+                double result = stod(altitude) * gr.at(search) / 5280;
+                std::cout << "\nThe estimated glide distance is " << result << " miles.\n\n";
+                break;
+            } catch (std::invalid_argument) {
+                std::cout << "\nEnter a valid altitude.\n\n";
+            }
+        }
+    }
 
-
- std::cout << "Enter a aircraft from index and a altitude to get the estimated glide distance.\n";
- std::cout << "Enter i for index and x to exit.\n";
-while(true){
-	  std::cout << "Enter a aircraft in index.\n";
-	  while(getline(std::cin,search)){
-	  auto i = gr.find(search);
-	  if(search == "x"){
-	  exit(EXIT_SUCCESS);}
-	  else if(search == "i"){
-	  ind();
-	  std::cout << "Enter a aircraft in index.\n";
-	  continue;}
-	  else if(i == gr.end()){
-	  std::cout << "\nEnter a aircraft in index only.\n\n";
-	  continue;}
-	  else{
-	  break;}
-	  }
-	  std::cout << "Enter a altitude.\n";
-	  while(getline(std::cin,altitude)){
-	  try{
-	  if(altitude == "i"){
-	  ind();
-	  std::cout << "Enter a altitude.\n"; 
-	  continue;}
-	  else if(altitude == "x"){
-	  exit(EXIT_SUCCESS);}
-	  double result = stod(altitude) *   gr.at(search) / 5280;
-          std::cout << "\nThe estimated glide distance is " << result << " miles.\n\n";
-	  break;
-	}catch(std::invalid_argument){
-	 std::cout << "Enter a valid altitude.\n\n";}                  
-	}
+    return 0;
 }
 
-return 0;
-}
